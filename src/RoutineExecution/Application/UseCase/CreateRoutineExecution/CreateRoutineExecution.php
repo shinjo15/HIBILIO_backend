08c9c28c2606ba69439df6b04e7cb045f646aecf
shinjo15 <shinjo0015@gmail.com>
2026-09-06 17:43:37 +0900

    Revert "refactor: separate routine execution from post creation"
    This reverts commit 4efdaf7bdf1236fb2fdbe21054d7910ce290fa62.

--- src/RoutineExecution/Application/UseCase/CreateRoutineExecution/CreateRoutineExecution.php
+++ src/RoutineExecution/Application/UseCase/CreateRoutineExecution/CreateRoutineExecution.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Src\RoutineExecution\Application\UseCase\CreateRoutineExecution;
 
+use Src\Post\Domain\Factory\PostFactoryInterface;
+use Src\Post\Domain\Repository\PostRepositoryInterface;
 use Src\Routine\Domain\Repository\RoutineActionRepositoryInterface;
 use Src\RoutineExecution\Domain\Factory\RoutineExecutionActionFactoryInterface;
 use Src\RoutineExecution\Domain\Factory\RoutineExecutionFactoryInterface;
@@ -20,7 +22,8 @@ final readonly class CreateRoutineExecution implements CreateRoutineExecutionInt
         private RoutineExecutionRepositoryInterface $routineExecutionRepository,
         private RoutineExecutionActionRepositoryInterface $routineExecutionActionRepository,
         private RoutineActionRepositoryInterface $routineActionRepository,
-
+        private PostFactoryInterface $postFactory,
+        private PostRepositoryInterface $postRepository,
     ) {}
 
     public function execute(CreateRoutineExecutionInputPort $input): void
@@ -43,6 +46,10 @@ final readonly class CreateRoutineExecution implements CreateRoutineExecutionInt
                 );
             }
 
+            $this->postRepository->save($this->postFactory->createActionPost(
+                $routineExecution->routineIdentifier(),
+                $routineExecution->routineExecutionIdentifier(),
+            ));
         });
     }
 
