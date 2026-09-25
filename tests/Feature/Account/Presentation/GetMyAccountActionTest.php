1b7bda158e523be01478a75de402baac44086e30
shinjo15 <shinjo0015@gmail.com>
2026-09-25 11:11:59 +0900

    test: #160の無関係な画像URLモックを削除

--- tests/Feature/Account/Presentation/GetMyAccountActionTest.php
+++ tests/Feature/Account/Presentation/GetMyAccountActionTest.php
@@ -6,7 +6,6 @@ namespace Tests\Feature\Account\Presentation;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\DB;
-use Src\Account\Application\Service\AccountImageUrlServiceInterface;
 use Src\Shared\Application\Service\AuthServiceInterface;
 use Src\Shared\Domain\ValueObject\Identifier\AccountIdentifier;
 use Tests\TestCase;
@@ -31,18 +30,6 @@ final class GetMyAccountActionTest extends TestCase
                 return '11111111-1111-4111-8111-111111111111';
             }
         });
-        $this->app->instance(AccountImageUrlServiceInterface::class, new class implements AccountImageUrlServiceInterface
-        {
-            public function iconImageUrl(AccountIdentifier $accountIdentifier): ?string
-            {
-                return 'https://images.example/icon';
-            }
-
-            public function headerImageUrl(AccountIdentifier $accountIdentifier): ?string
-            {
-                return 'https://images.example/header';
-            }
-        });
 
         $this->getJson('/api/my/account')
             ->assertOk()
@@ -51,8 +38,8 @@ final class GetMyAccountActionTest extends TestCase
                 'account_name' => 'ログインアカウント',
                 'account_bio' => '自己紹介',
                 'visibility' => 'public',
-                'icon_image_url' => 'https://images.example/icon',
-                'header_image_url' => 'https://images.example/header',
+                'icon_image_url' => null,
+                'header_image_url' => null,
                 'ui_mode' => 'system',
                 'favorite_tags' => [[
                     'tag_identifier' => 'aaaaaaaa-aaaa-4aaa-8aaa-aaaaaaaaaaaa',
